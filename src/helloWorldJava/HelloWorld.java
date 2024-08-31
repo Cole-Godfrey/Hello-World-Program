00bf6936fb787b5483b1a9cb4729fb02a7f04155
Cole <colegodfrey34@gmail.com>
2024-08-30 23:14:15 -0700

    Initial Commit

---
+++ src/helloWorldJava/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloWorldJava;
+
+// My first program
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module helloWorldJava {
+}
